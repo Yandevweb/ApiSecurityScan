a7544a94dabd7f47fe06948751be71228f98aa9c
haikizoku <gnorvene@gmail.com>
2018-02-05 15:24:59 +0100

    mise en place des mails avec multi pièces jointes

--- app/Http/Controllers/Api/ControllerMail.php
+++ app/Http/Controllers/Api/ControllerMail.php
@@ -8,27 +8,66 @@ use Mail;
 
 class ControllerMail extends Controller
 {
+
     public function sendEmail()
     {
-     $data = array(
-        'name' => "app-secure-scan");
-        $emails = ['enfoux.kevin@gmail.com','gnorvene@gmail.com','gregory.norvene@laposte.net','yannick.jeanjean.pro@gmail.com'];
-        try
-        {
 
-            Mail::send('emails.welcome', $data, function ($message)use ($emails, $data) {
-            $message->from('gnorvene2@gmail.com', 'result php');
-            $message->to($emails)->subject('Report  test');
+        $from = 'scanphp@gmail.com';
+        $data = array(
+            'name' => "app-secure-scan");
+        $emails = ['enfoux.kevin@gmail.com', 'gnorvene@gmail.com', 'gregory.norvene@laposte.net', 'yannick.jeanjean.pro@gmail.com'];
+        chdir('../tmp');
+        $projectPath = getcwd();
+
+        //TODO Recupere le chemin de chaque fichier et les ajoute dans un array
+        $requestA = "test/testphpca.txt";
+        $requestB = "test/testphpcaf.txt";
+        $pathToFileA = $projectPath . '/' . $requestA;
+        $pathToFileB = $projectPath . '/' . $requestB;
+
+        $pathToFile = [
+            $pathToFileA,
+            $pathToFileB,
+        ];
+
+        try {
+
+            Mail::send('emails.welcome', $data, function ($message) use ($emails, $data, $pathToFile, $from) {
+
+                $message->from($from, 'result php');
+                $message->to($emails)->subject('Report  test');
+
+                //Parcourt le tableau pour générer chaque pièce jointe
+                foreach($pathToFile as  $path){
+                    $message->attach($path);
+                }
 
             });
+
+        } catch (\Exception $e) {
+            ($e->getMessage());
         }
-        catch (\Exception $e)
-        {
-            dd($e->getMessage());
-        }
-            return response()->json(['status'=>'success', 'return' => "Your email has been sent successfully"]);
+
+        return response()->json(['status' => 'success', 'return' => "Your email has been sent successfully"]);
 
     }
+
+}
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    /*public function build()
+    {
+        return $this->view('emails.welcome')
+            ->attachData($pathToFile, 'name.txt', [
+                'mime' => 'plain/text',
+            ]);
+    }
+
+
+
 
 
 }
